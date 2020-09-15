957b10a38e07bbc3ac047989fe098db159bfc2aa
Leandro Lopes <lopesleandro.it@gmail.com>
2020-09-15 16:16:37 -0300

    create view index with ejs - embedded JavaScript templating

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const Koa = require('koa');
 const startDb = require('./db');
 const port = process.env.PORT || 3000;
 const router = require('./middleware/router');
+const views = require('koa-views');
 
 // inicializa o banco de dados
 startDb();
 
 // cria a instancia do app
 const app = new Koa();
+
+// registrado antes de router para que ctx.render esteja disponível
+// para as definições da rota
+app.use(views(`${__dirname}/views`, {
+  extension: 'ejs'
+}));
 
 // rotas
 app.use(router.routes());
